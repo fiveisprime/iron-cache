780c9a1051dc2bfd817cba26a1c71809d258572d
Matt Hernandez <matt@modulus.io>
2013-12-12 19:55:19 -0500

    Full API implementation and tests.
    Includes cache and key management with tests.

--- spec/ironcache-spec.js
+++ spec/ironcache-spec.js
@@ -11,25 +11,106 @@ describe('initialization', function() {
 
 });
 
-describe('API calls', function() {
+describe('cache API calls', function() {
 
-  it('should call `list` with the correct parameters', function() {
+  it('should call `list` with the correct parameters', function(done) {
     var ic = ironcache('list', '123', function(uri, token, method) {
       uri.should.equal('/projects/list/caches');
       token.should.equal('123');
       method.should.equal('GET');
+
+      done();
     });
 
     ic.list();
   });
-  
-  it('should call `get` with the correct parameters', function() {
-    var ic = ironcache('get', '456', function(uri, token, method) {
-      uri.should.equal('/projects/get/caches/cache');
+
+  it('should call `info` with the correct parameters', function(done) {
+    var ic = ironcache('info', '456', function(uri, token, method) {
+      uri.should.equal('/projects/info/caches/info-cache');
       token.should.equal('456');
       method.should.equal('GET');
+
+      done();
     });
 
-    ic.get('cache');
+    ic.info('info-cache');
+  });
+
+  it('should call `delCache` with the correct parameters', function(done) {
+    var ic = ironcache('delCache', '789', function(uri, token, method) {
+      uri.should.equal('/projects/delCache/caches/del-cache');
+      token.should.equal('789');
+      method.should.equal('DELETE');
+
+      done();
+    });
+
+    ic.delCache('del-cache');
+  });
+
+  it('should call `clearCache` with the correct parameters', function(done) {
+    var ic = ironcache('clearCache', '1011', function(uri, token, method) {
+      uri.should.equal('/projects/clearCache/caches/clr-cache/clear');
+      token.should.equal('1011');
+      method.should.equal('POST');
+
+      done();
+    });
+
+    ic.clearCache('clr-cache');
+  });
+});
+
+describe('key API calls', function() {
+
+  it('should call `put` with the correct parameters', function(done) {
+    var ic = ironcache('put', '1213', function(uri, token, method, data) {
+      uri.should.equal('/projects/put/caches/put-cache/items/key-name');
+      token.should.equal('1213');
+      method.should.equal('PUT');
+
+      data.should.have.property('test');
+
+      done();
+    });
+
+    ic.put('put-cache', 'key-name', { test: true });
+  });
+
+  it('should call `incr` with the correct parameters', function(done) {
+    var ic = ironcache('incr', '1415', function(uri, token, method) {
+      uri.should.equal('/projects/incr/caches/incr-cache/items/key-name/increment');
+      token.should.equal('1415');
+      method.should.equal('POST');
+
+      done();
+    });
+
+    ic.incr('incr-cache', 'key-name');
+  });
+  
+  it('should call `get` with the correct parameters', function(done) {
+    var ic = ironcache('get', '1617', function(uri, token, method) {
+      uri.should.equal('/projects/get/caches/get-cache/items/key-name');
+      token.should.equal('1617');
+      method.should.equal('GET');
+
+      done();
+    });
+
+    ic.get('get-cache', 'key-name');
+  });
+  
+  it('should call `del` with the correct parameters', function(done) {
+    var ic = ironcache('del', '1819', function(uri, token, method) {
+      uri.should.equal('/projects/del/caches/del-cache/items/key-name');
+      token.should.equal('1819');
+      method.should.equal('DELETE');
+
+      done();
+    });
+
+    ic.del('del-cache', 'key-name');
   });
 });
